1d41a2f8f2257c053a4e288eaa03a3d9258440a3
Roman Yehorov <roman.yehorov@student.tuke.sk>
2024-09-22 15:42:37 +0200

    add domain layer

---
+++ src/main/java/com/javarush/domain/City.java
@@ -0,0 +1,23 @@
+package com.javarush.domain;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(schema = "world", name = "city")
+public class City {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "country_id")
+    private Country country;
+
+    private String district;
+
+    private int population;
+}
