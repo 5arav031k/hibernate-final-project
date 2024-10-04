766ba9659581857f3f357a389a1392fe4a3215bd
Roman Yehorov <roman.yehorov@student.tuke.sk>
2024-10-04 20:40:30 +0200

    feat: add classes for redis

---
+++ src/main/java/com/javarush/redis/Language.java
@@ -0,0 +1,18 @@
+package com.javarush.redis;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Language {
+    private String language;
+
+    private Boolean isOfficial;
+
+    private BigDecimal percentage;
+}
