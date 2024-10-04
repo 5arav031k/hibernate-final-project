56c5f6c4bffb32af84498230dd7901092890c3b1
Roman Yehorov <roman.yehorov@student.tuke.sk>
2024-10-04 22:17:10 +0200

    refactor: small refactor

--- src/main/java/com/javarush/redis/Language.java
+++ src/main/java/com/javarush/redis/Language.java
@@ -1,14 +1,10 @@
 package com.javarush.redis;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.Data;
 
 import java.math.BigDecimal;
 
-@Getter
-@Setter
-@NoArgsConstructor
+@Data
 public class Language {
     private String language;
 
