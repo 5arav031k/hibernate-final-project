53c57174c82dc732bb4cad1e7b4d923fc1f8d630
Roman Yehorov <roman.yehorov@student.tuke.sk>
2024-10-04 22:28:06 +0200

    perf: improve performance using jedis and gson

--- src/main/java/com/javarush/Main.java
+++ src/main/java/com/javarush/Main.java
@@ -1,38 +1,60 @@
 package com.javarush;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
 import com.javarush.config.HibernateUtil;
 import com.javarush.domain.entity.City;
 import com.javarush.domain.entity.Country;
+import com.javarush.domain.entity.CountryLanguage;
+import com.javarush.redis.CityCountry;
+import com.javarush.redis.Language;
 import com.javarush.repository.CityRepository;
 import com.javarush.repository.CountryRepository;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import redis.clients.jedis.JedisPooled;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Main {
-
-    private final SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
-//    private final RedisClient redisClient;
-
-    private final ObjectMapper mapper;
+    private final JedisPooled jedis;
+    private final Gson gson;
 
     private final CityRepository cityRepository;
     private final CountryRepository countryRepository;
 
+    private final SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+
     public Main() {
         cityRepository = new CityRepository();
         countryRepository = new CountryRepository();
-
-//        redisClient = prepareRedisClient();
-        mapper = new ObjectMapper();
+        gson = new Gson();
+        jedis = new JedisPooled("localhost", 6379);
     }
 
     public static void main(String[] args) {
         Main main = new Main();
+
         List<City> allCities = main.fetchData(main);
+        List<CityCountry> preparedData = main.transformData(allCities);
+        main.pushToRedis(preparedData);
+        main.sessionFactory.getCurrentSession().close();
+
+        List<Integer> ids = List.of(3, 2545, 123, 4, 189, 89, 3458, 1189, 10, 102);
+
+        long startRedis = System.currentTimeMillis();
+        main.testRedisData(ids);
+        long stopRedis = System.currentTimeMillis();
+
+        long startMysql = System.currentTimeMillis();
+        main.testMysqlData(ids);
+        long stopMysql = System.currentTimeMillis();
+
+        System.out.printf("%s:\t%d ms\n", "Redis", (stopRedis - startRedis));
+        System.out.printf("%s:\t%d ms\n", "MySQL", (stopMysql - startMysql));
+
         HibernateUtil.shutdown();
     }
 
@@ -41,7 +63,7 @@ public class Main {
             List<City> allCities = new ArrayList<>();
 
             session.beginTransaction();
-            List<Country> countries = main.countryRepository.getAll();
+            main.countryRepository.getAll();
 
             long totalCount = main.cityRepository.getCount();
             int step = 500;
@@ -50,6 +72,60 @@ public class Main {
             }
             session.getTransaction().commit();
             return allCities;
+        }
+    }
+
+    private List<CityCountry> transformData(List<City> cities) {
+        return cities.stream().map(city -> {
+            CityCountry res = new CityCountry();
+            res.setId(city.getId());
+            res.setName(city.getName());
+            res.setPopulation(city.getPopulation());
+            res.setDistrict(city.getDistrict());
+
+            Country country = city.getCountry();
+            res.setAlternativeCountryCode(country.getAlternativeCode());
+            res.setContinent(country.getContinent());
+            res.setCountryCode(country.getCode());
+            res.setCountryName(country.getName());
+            res.setCountryPopulation(country.getPopulation());
+            res.setCountryRegion(country.getRegion());
+            res.setCountrySurfaceArea(country.getSurfaceArea());
+            Set<CountryLanguage> countryLanguages = country.getLanguages();
+            Set<Language> languages = countryLanguages.stream().map(cl -> {
+                Language language = new Language();
+                language.setLanguage(cl.getLanguage());
+                language.setIsOfficial(cl.isOfficial());
+                language.setPercentage(cl.getPercentage());
+                return language;
+            }).collect(Collectors.toSet());
+            res.setLanguages(languages);
+
+            return res;
+        }).collect(Collectors.toList());
+    }
+
+    private void pushToRedis(List<CityCountry> data) {
+        for (CityCountry cityCountry : data) {
+            jedis.set(String.valueOf(cityCountry.getId()), gson.toJson(cityCountry));
+        }
+    }
+
+    private void testRedisData(List<Integer> ids) {
+        for (Integer id : ids) {
+            String value = jedis.get(String.valueOf(id));
+            gson.fromJson(value, CityCountry.class);
+        }
+    }
+
+    private void testMysqlData(List<Integer> ids) {
+        try (Session session = sessionFactory.getCurrentSession()) {
+            session.beginTransaction();
+            for (Integer id : ids) {
+                City city = cityRepository.getById(id);
+                Set<CountryLanguage> ignored = city.getCountry().getLanguages();
+            }
+            session.getTransaction().commit();
         }
     }
 }
